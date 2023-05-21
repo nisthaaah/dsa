ad0171fea859d5057c10a9e5eb5cabda438f86f3
nistha <nisthasharma0799@gmail.com>
2023-05-21 20:18:22 +0530

    final

---
+++ recursion/Recursion2.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class Recursion2 {
+    public static void printn(int n){
+        if(n==5){
+            return;
+        }
+        System.out.println(n);
+        printn(n+1);
+    }
+    public static void main(String args[]){
+        int n=1;
+        printn(n);
+    }
+
+    
+}
---
+++ recursion/recursion3.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class recursion3 {
+    public static void printsum(int i, int n, int sum){
+        if(i==n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        printsum(i+1, n, sum);
+    }
+    public static void main(String args[]){
+        printsum(1, 6, 0);
+    }
+    
+}
