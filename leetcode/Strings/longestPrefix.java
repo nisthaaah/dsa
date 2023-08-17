e42b1ae3a9b15caa236c58d1c22628071a3f2cb5
nistha <nisthasharma0799@gmail.com>
2023-08-18 00:43:57 +0530

    leetcode

---
+++ leetcode/Strings/longestPrefix.java
@@ -0,0 +1,26 @@
+package leetcode.Strings;
+
+import java.util.Arrays;
+
+public class longestPrefix {
+    public static String longestPrefix(String[] arr){
+        Arrays.sort(arr); //{flight, flow, flower}
+        String s1 = arr[0];
+        String s2 = arr[arr.length - 1];
+        int i = 0;
+        while(i < s1.length() && i < s2.length()){
+            if(s1.charAt(i) == s2.charAt(i)){
+                i++;
+            }
+            else break;
+        }
+        return s1.substring(0,i);
+    
+    }
+
+    public static void main(String[] args){
+        String[] arr = {"flower","flow","flight"};
+        System.out.println("Longest Prefix: " +longestPrefix(arr));
+    }
+    
+}
