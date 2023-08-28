51efba32dad1dfbf33b531b442785c06098a5385
nistha <nisthasharma0799@gmail.com>
2023-08-29 01:02:31 +0530

    leetcode

--- leetcode/LinkedList/addTwoNumbers.java
+++ leetcode/LinkedList/addTwoNumbers.java
@@ -37,7 +37,7 @@ public class addTwoNumbers {
     public void printList(Node head) {
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.value +" -> ");
+            System.out.print(temp.value + " -> ");
             temp = temp.next;
         }
         System.out.print("null\n");
