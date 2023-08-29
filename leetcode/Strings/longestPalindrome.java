7a9e810523e2b16e26f81730ab0039157e8858c3
nistha <nisthasharma0799@gmail.com>
2023-08-30 00:35:43 +0530

    leetcode

---
+++ leetcode/Strings/longestPalindrome.java
@@ -0,0 +1,43 @@
+package leetcode.Strings;
+
+public class longestPalindrome {
+    public String longestPalindrome(String s){
+        int start = 0;
+        int maxLen = 0;
+
+        for(int i = 1; i < s.length(); i++){
+
+            //odd
+            int left = i-1;
+            int right = i+1;
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                int len = right - left + 1;
+                if(len > maxLen){
+                    maxLen = len;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+
+            //even
+            left = i-1; 
+            right = i;
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                int len = right - left + 1;
+                if(len > maxLen){
+                    maxLen = len;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+        } //Expand Around Center approach
+        return s.substring(start, start + maxLen);
+    }
+
+    public static void main(String[] args){
+        String s = "caac";
+        System.out.println("The longest palindromic Substring is: " +new longestPalindrome().longestPalindrome(s));
+    }    
+}
