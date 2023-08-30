ca4f77972ce690e010b62930dd6e5dd2d055b45d
nistha <nisthasharma0799@gmail.com>
2023-08-30 23:10:11 +0530

    leetcode

--- leetcode/Strings/longestPalindrome.java
+++ leetcode/Strings/longestPalindrome.java
@@ -37,7 +37,7 @@ public class longestPalindrome {
     }
 
     public static void main(String[] args){
-        String s = "caac";
+        String s = "cabad";
         System.out.println("The longest palindromic Substring is: " +new longestPalindrome().longestPalindrome(s));
     }    
 }
