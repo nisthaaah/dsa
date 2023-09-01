18ce053bb3d491b6cb35b182627648c4b6684830
nistha <nisthasharma0799@gmail.com>
2023-09-01 15:16:17 +0530

    leetcode

---
+++ leetcode/Random/bulbSwitcher.java
@@ -0,0 +1,17 @@
+package leetcode.Random;
+
+public class bulbSwitcher {
+    public int bulb(int n){
+        return (int) Math.sqrt(n);  //2.44 => 2
+    }
+
+    public static void main(String[] args){
+        int n = 6;
+        System.out.println("The number of bulbs that are on after " +n+ " rounds: " +new bulbSwitcher().bulb(n) );
+    } 
+} //on on on on on on         
+ //on off on off on off
+ //on off off off on on
+ //on off off on on on
+ //on off off on off on
+ //on off off on off off
