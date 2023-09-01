cece929df6024b47c228b5bcc8e9e8c5d9ac6f87
nistha <nisthasharma0799@gmail.com>
2023-09-01 13:47:07 +0530

    leetcode

--- leetcode/Random/addDigits.java
+++ leetcode/Random/addDigits.java
@@ -13,6 +13,10 @@ public class addDigits {
             }
         }
         return sum; //6
+
+        // if (num == 0) return 0;
+        // if (num % 9 == 0) return 9; // 78 % 9 == 6
+        // return num % 9;
     }
     public static void main(String[] args){
         int num = 78;
