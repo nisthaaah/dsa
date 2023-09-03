94348ea6a9f3b23f20ec05ab6951290673cff1bb
nistha <nisthasharma0799@gmail.com>
2023-09-03 17:03:10 +0530

    leetcode

--- leetcode/Random/containerWithMostWater.java
+++ leetcode/Random/containerWithMostWater.java
@@ -3,17 +3,17 @@ package leetcode.Random;
 public class containerWithMostWater {
     public int maxArea(int[] height){
         int left = 0;
-        int right = height.length - 1;
+        int right = height.length - 1; //5
         int maxArea = 0;
 
         while(left < right){
-            int currArea = Math.min(height[left], height[right]) * (right - left);
-            maxArea = Math.max(maxArea, currArea);
-
-            if(height[left] < height[right]) left++;
-            else right--;
+            int currArea = Math.min(height[left], height[right]) * (right - left); //(1, 3) * 5 => 5  
+            maxArea = Math.max(maxArea, currArea);                                //(2, 3) * 4 => 8
+                                                                                 //(5, 3) * 3 => 9
+            if(height[left] < height[right]) left++; //left++                   //(5, 8) * 2 => 10        
+            else right--; //right--                                            //(8, 9) * 1 => 8
         }
-        return maxArea;
+        return maxArea; //10
     }
     public static void main(String[] args){
         int[] height = {1,2,5,8,9,3};
---
+++ leetcode/Strings/generateParentheses.java
@@ -0,0 +1,27 @@
+package leetcode.Strings;
+import java.util.*;
+
+public class generateParentheses {
+    public List<String> generateParentheses(int n){
+        List<String> res = new ArrayList<>();
+        backTrack(res, 0, 0, "", n);
+        return res;
+    }
+
+    public void backTrack(List<String> res, int open_count, int close_count, String s, int n){
+        if(s.length() == n * 2){
+            res.add(s);
+            return;
+        }
+        if(open_count < n){
+            backTrack(res, open_count + 1, close_count, s + "(", n);
+        }
+        if(close_count < open_count){
+            backTrack(res, open_count, close_count + 1, s + ")", n);
+        }
+    }
+    public static void main(String[] args){
+        int n = 3;
+        System.out.println("The generated parentheses for " +n+ " are: " +new generateParentheses().generateParentheses(n));
+    }   
+}
