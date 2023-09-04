6dd1381709dc7d11c95d829533a975fdf570351c
nistha <nisthasharma0799@gmail.com>
2023-09-04 21:48:39 +0530

    leetcode

--- leetcode/HashMap/missingNumber.java
+++ leetcode/HashMap/missingNumber.java
@@ -3,21 +3,29 @@ import java.util.*;
 
 public class missingNumber {
     public int missingNumber(int[] nums){
-        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+        int expectedSum = (n * (n + 1)) / 2;
+        int currSum = 0;
 
-        for(int i = 0; i < nums.length; i++){ //3
-            numMap.put(nums[i], nums[i]); // (3,3) , (0,0), (1,1)
+        for(int i =0; i < nums.length; i++){
+            currSum += nums[i];
         }
+        return expectedSum - currSum;
+    //     Map<Integer, Integer> numMap = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
-        if(numMap.containsKey(i)){
-            continue;
-        }
-        else{
-            return i; //2
-        }
-    }
-    return nums.length;
+    //     for(int i = 0; i < nums.length; i++){ //3
+    //         numMap.put(nums[i], nums[i]); // (3,3) , (0,0), (1,1)
+    //     }
+
+    //     for(int i = 0; i < nums.length; i++){
+    //     if(numMap.containsKey(i)){
+    //         continue;
+    //     }
+    //     else{
+    //         return i; //2
+    //     }
+    // }
+    // return nums.length; //If none of the numbers from 0 to nums.length, returns nums.length (4)
 }
 
 public static void main(String[] args){
