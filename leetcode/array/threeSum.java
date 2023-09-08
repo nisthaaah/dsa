4afc73dd49e89d00c8d27f2da7c30a5d73d570c2
nistha <nisthasharma0799@gmail.com>
2023-09-08 19:26:36 +0530

    leetcode

---
+++ leetcode/array/threeSum.java
@@ -0,0 +1,52 @@
+package leetcode.Array;
+import java.util.*;
+
+public class threeSum {
+    public List<List<Integer>> threeSum(int[] arr){
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(arr); //[-4, -1, -1, 0, 1, 2]
+
+        for(int i = 0; i < arr.length - 2; i++){ //1
+            if(i > 0 && arr[i] == arr[i -1]) continue; //skip duplicates
+
+            int j = i + 1;  
+            int k = arr.length - 1; 
+
+            while(j < k){
+                int sum = arr[i] + arr[j] + arr[k]; 
+
+                if(sum == 0){
+                    res.add(Arrays.asList(arr[i], arr[j], arr[k]));
+
+                    while(j < k && arr[j] == arr[j+1]){
+                        j++;
+                    }
+                    while(j < k && arr[k] == arr[k - 1]){
+                        k--;
+                    }
+                    j++;
+                    k--;
+                }
+                else if(sum < 0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {0, 1, -1, -1, 2, -4};
+
+        List<List<Integer>> res = new threeSum().threeSum(arr);
+
+        for(List<Integer> triplet : res){
+            System.out.println(triplet);
+
+        }
+    }
+}
---
+++ leetcode/array/threeSumClosest.java
@@ -0,0 +1,38 @@
+package leetcode.Array;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class threeSumClosest {
+    public int threeSumClosest(int[] arr, int target){
+        Arrays.sort(arr);
+        int closestSum = arr[0] + arr[1] +arr[2];
+
+        for(int i = 0; i < arr.length - 2; i++){
+            int j = i + 1;
+            int k = arr.length - 1;
+
+            while(j < k){
+                int sum = arr[i] + arr[j] + arr[k];
+
+                if(Math.abs(target - sum) < Math.abs(target - closestSum)){
+                    closestSum = sum;
+                }
+                if(sum < target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return closestSum;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {-1,2,1,-4};
+        int target = 1;
+
+        System.out.println("The three sum closest is: " +new threeSumClosest().threeSumClosest(arr, target));
+    }
+}
