8fdf2c742d197fc147263d45493d30479ab1bff0
nistha <nisthasharma0799@gmail.com>
2023-09-09 19:07:33 +0530

    Exception Handling

---
+++ exception handling/Eg1.java
@@ -0,0 +1,29 @@
+import java.io.FileInputStream;
+
+public class Eg1 {
+    public static void main(String[] args){
+    try{
+        FileInputStream fis = new FileInputStream("c: abc.txt");
+        Class.forName("com.mysq.jdbc.Driver"); 
+    }
+    catch(Exception e){
+        System.out.println(e);
+    } //Compile time exception
+    
+    int a= 100, b= 0, c;
+    c = a/b;
+    System.out.println(c); //Run time exception
+
+
+    try{
+    String s = null;
+    System.out.println(s.length()); //Run time exception
+    }
+    catch(NullPointerException e){
+        System.out.println(e);
+    } //exception handling
+
+    System.out.println("a");
+
+    }
+}
