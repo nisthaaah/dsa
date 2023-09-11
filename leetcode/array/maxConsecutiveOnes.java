1cbe520bd9beea2807024081b01814f99d718e6e
nistha <nisthasharma0799@gmail.com>
2023-09-11 23:52:30 +0530

    leetcode

--- leetcode/array/maxConsecutiveOnes.java
+++ leetcode/array/maxConsecutiveOnes.java
@@ -4,7 +4,7 @@ public class maxConsecutiveOnes {
     public int maxConsecutiveOnes(int[] arr){
         int count = 0;
         int maxCount = 0;
-        for(int i = 0; i < arr.length; i++){ //7
+        for(int i = 0; i < arr.length; i++){ 
             if(arr[i] == 1) count++;
             else count = 0;
 
