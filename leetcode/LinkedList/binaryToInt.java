03c01d96a81143e7993ef1de10c4e30315598978
nistha <nisthasharma0799@gmail.com>
2023-09-14 22:04:20 +0530

    leetcode

--- leetcode/LinkedList/binaryToInt.java
+++ leetcode/LinkedList/binaryToInt.java
@@ -15,7 +15,7 @@ public class binaryToInt {
     public int binaryToInt(Node head){
         int sum = 0;
         while(head != null){
-            sum = (sum << 1) + head.value; // 0 * 2 + 1 => 1 || 1 * 2 + 0 => 2 || 2 * 2 + 1 => 5
+            sum = (sum << 1) + head.value; //0 * 2 + 1 => 1 || 1 * 2 + 0 => 2 || 2 * 2 + 1 => 5
             head = head.next;
         }
         return sum; //5
@@ -24,6 +24,7 @@ public class binaryToInt {
         Node head = new Node(1);
         head.next = new Node(0);
         head.next.next = new Node(1);
+        //head.next.next.next = new Node(1);
 
         System.out.println("The decimal value of the number in the linked list is: " +new binaryToInt().binaryToInt(head));
     }
