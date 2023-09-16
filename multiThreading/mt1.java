d574f6f057fc5d2a9608667707ea1787146c81ca
nistha <nisthasharma0799@gmail.com>
2023-09-16 21:17:09 +0530

    multi threading

--- multiThreading/mt1.java
+++ multiThreading/mt1.java
@@ -1,14 +1,24 @@
 package multiThreading;
 
-class Test extends Thread{
+class Test1 extends Thread{
     public void run(){
-        System.out.println("Thread created");
+        System.out.println("Task 1");
+    }
+}
+class Test2  extends Thread{
+    public void run(){
+        System.out.println("Task 2");
     }
 }
 public class mt1 {
-    public static void main(String[] args){
-        Test t = new Test();
-        t.start();
+    public static void main(String[] args){ //main thread(JVM)
+        Test1 t1 = new Test1(); //2nd thread
+        t1.start();
+        Test1 t2 = new Test1(); //3rd thread
+        t2.start();
+
+        Test2 t21 = new Test2(); //4th thread
+        t21.start();
         //t.start();
     }
 }
