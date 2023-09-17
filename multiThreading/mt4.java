3f084301593c378a8c246e51fa7279075f6d3f51
nistha <nisthasharma0799@gmail.com>
2023-09-17 22:08:28 +0530

    multi threading

--- multiThreading/mt4.java
+++ multiThreading/mt4.java
@@ -4,9 +4,11 @@ class TestD extends Thread{
     public void run(){
         if(Thread.currentThread().isDaemon()){
             System.out.println("Daemon thread");
+            System.out.println("Daemon Thread priority: " +Thread.currentThread().getPriority());
         }
         else{
-            System.out.println("Child thread");
+            Thread.currentThread().setPriority(10);
+            System.out.println("Child Thread priority: " +Thread.currentThread().getPriority());
         }
         
     }
@@ -15,8 +17,9 @@ public class mt4 {
     public static void main(String[] args){
         //Thread.currentThread().setDaemon(true); //not possible
         System.out.println("Main thread");
+        System.out.println("Main Thread priority: " +Thread.currentThread().getPriority());
         TestD td = new TestD();
-        td.setDaemon(true);
+        //td.setDaemon(true);
         td.start();
     }
 }
