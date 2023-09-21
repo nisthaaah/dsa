f3fbb9c61433a1f3a7449e84fb3d99982e3a97f6
nistha <nisthasharma0799@gmail.com>
2023-09-21 20:46:06 +0530

    multi threading

---
+++ multiThreading/syncronization/BankAccount.java
@@ -0,0 +1,45 @@
+package multiThreading.syncronization;
+
+//Using Synchronization block
+class Bank {
+   double balance = 2000;
+   void withdraw(double amount){
+        System.out.println("WELCOME TO ABC BANK " +Thread.currentThread().getName());
+
+        synchronized(this){
+            if(balance >= amount){
+            System.out.println(amount +" is withdrawing...");
+            balance = balance - amount; 
+            System.out.println("Balance after withdrawal: " +balance +"\n"); 
+        }
+        else{
+            System.out.println("Insufficient Balance!! ");
+            System.out.println("Balance left: " +balance);
+        }
+        }
+
+        System.out.println("WELCOME TO ABC BANK1 " +Thread.currentThread().getName());
+        System.out.println("WELCOME TO ABC BANK1 " +Thread.currentThread().getName());
+        System.out.println("WELCOME TO ABC BANK1 " +Thread.currentThread().getName());
+    }
+}
+
+public class BankAccount extends Thread{
+    static Bank ba;
+    double amount;
+    public void run(){
+        ba.withdraw(amount);
+    }   
+
+    public static void main(String[] args){
+        ba = new Bank();
+
+        BankAccount withdrawal1 = new BankAccount();
+        withdrawal1.amount = 300;
+        withdrawal1.start();
+
+        BankAccount withdrawal2 = new BankAccount();
+        withdrawal2.amount = 2000;
+        withdrawal2.start();
+    }
+}
--- multiThreading/syncronization/movieBookApp.java
+++ multiThreading/syncronization/movieBookApp.java
@@ -1,20 +1,21 @@
 package multiThreading.syncronization;
 
+//Using Synchronization Method
 class BookTheaterSeat{
     int totalSeats = 10;
    synchronized void bookSeat(int seats){
         if(totalSeats >= seats){
-            System.out.println(seats+ " Seats booked successfully");
+            System.out.println(seats+ " Seats booked successfully :)");
             totalSeats = totalSeats - seats; //10 - 5 => 5
-            System.out.println("Seats left: " +totalSeats); //5
+            System.out.println("Seats left: " +totalSeats +"\n"); //5
         }
         else{
-            System.out.println("Seats cannot be booked");
+            System.out.println("Sorry, " +seats+ " seats cannot be booked....!!");
             System.out.println("Seats left: " +totalSeats);
         }
     }
 }
-public class movieBookApp extends Thread{
+public class MovieBookApp extends Thread{
     static BookTheaterSeat b;
     int seats;
     public void run(){
@@ -24,11 +25,11 @@ public class movieBookApp extends Thread{
     public static void main(String[] args){
         b = new BookTheaterSeat();
 
-        movieBookApp user1 = new movieBookApp();
+        MovieBookApp user1 = new MovieBookApp();
         user1.seats = 5;
         user1.start();
 
-        movieBookApp user2 = new movieBookApp();
+        MovieBookApp user2 = new MovieBookApp();
         user2.seats = 7;
         user2.start();
     }
