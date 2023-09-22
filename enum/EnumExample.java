f5c2c5088b668589508625b9c382290978d44179
nistha <nisthasharma0799@gmail.com>
2023-09-22 22:31:33 +0530

    enum

--- enum/EnumExample.java
+++ enum/EnumExample.java
@@ -1,19 +1,26 @@
 public class EnumExample {
-    enum Month{
+    enum Month implements A{
         January, February, March, April, May, June, July, August, September, October, November, December;
         //these are enum constants
         //are implicitly public, static, final
         //enums cannot be extended using inheritance
         //You cannot create new instances of an enum using the new keyword, and you cannot extend an enum
-
+        //can implement interface
+    
     Month(){
         System.out.println("Constructor " +this);
+    }
+
+    @Override
+    public void hey() {
+        System.out.println("HEY!!");
     } 
     } 
 
     public static void main(String[] args) {
         Month month;
         month = Month.December;
+        month.hey();
 
         // for(Month m : Month.values()){
         //     System.out.println(m);
