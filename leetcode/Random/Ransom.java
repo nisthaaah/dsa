dd276f164331fce7533a24464fea6b79f158cbdc
nistha <nisthasharma0799@gmail.com>
2023-09-23 23:43:32 +0530

    leetcode

--- leetcode/Random/Ransom.java
+++ leetcode/Random/Ransom.java
@@ -4,27 +4,23 @@ public class Ransom {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] charCount = new int[26]; 
 
-        // Count the character frequencies in the magazine
         for (char c : magazine.toCharArray()) { //aab
-            charCount[c - 'a']++; //charCount[]
+            charCount[c - 'a']++; //to convert a lowercase character c into an index relative to the alphabet
+        } //charCount[0] -> 2, charCount[1] -> 1
+        
+        for (char c : ransomNote.toCharArray()) { //aa
+            int index = c - 'a'; //0
+            if (charCount[index] == 0) return false;
+            charCount[index]--; //charCount[0] -> 1, 0
         }
 
-        // Check if the ransom note can be constructed
-        for (char c : ransomNote.toCharArray()) {
-            int index = c - 'a';
-            if (charCount[index] == 0) {
-                return false;
-            }
-            charCount[index]--;
-        }
-
-        return true;
+        return true; //true, ransomNote can be constructed by using magazine
     }
 
     public static void main(String[] args) {
-        String ransomNote = "aa";
-        String magazine = "aab";
+        String ransomNote = "abb"; //to be constructed
+        String magazine = "aab"; //to be used for constructing
 
-        System.out.println("Each letter in magazine can only be used once in ransomNote: " +new Ransom().canConstruct(ransomNote, magazine) );
+        System.out.println("Can each letter in magazine be used to construct ransomNote: " +new Ransom().canConstruct(ransomNote, magazine) );
     }
 }
