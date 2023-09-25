369751ed2dcd71343050c56dddf24774485fdbf4
nistha <nisthasharma0799@gmail.com>
2023-09-25 23:43:40 +0530

    binary search

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -2,39 +2,23 @@ import java.util.*;
 class BinarySearch {
     int binarySearch(int arr[], int x, int low, int high){
         if(low <= high){
-        int mid= (low+high)/2;
-            if(arr[mid]==x){
-                return mid+1;
+        int mid= (low+high)/2; //(0+4) / 2 => 2
+            if(arr[mid] == x){
+                return mid + 1; //3
             }
             else if(x > arr[mid]){
-                return binarySearch(arr,mid+1,high,x);
+                return binarySearch(arr, mid+1, high, x);
         }
             else{
-                return binarySearch(arr,low,mid-1,x);
+                return binarySearch(arr, low, mid-1, x);
         }
     }
-        return -1;
+    return -1;
 }
        
         public static void main(String[] args){
-            BinarySearch bs=new BinarySearch();
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter the size: ");
-            int size=sc.nextInt();
-            int arr[]=new int[size];
-            System.out.println("Enter the elements: ");
-            for(int i=0; i<size; i++){
-                arr[i]=sc.nextInt();
-            }
+            int[] arr = {1,2,3,4,5};
 
-            System.out.println("Enter the target: ");
-            int x=sc.nextInt();
-            int result=bs.binarySearch(arr,x,0,size-1);
-            if(result==-1){
-                System.out.println("not found");
-            }
-                else{
-                    System.out.println("found at: " +result);
-                }
+            System.out.println("The element is " +new BinarySearch().binarySearch(arr, 3, 0, 4));
         }
     }
