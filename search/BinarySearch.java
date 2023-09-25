374dd50abd936eb1ec4cbddcbf2e3b8c30d2ee3d
nistha <nisthasharma0799@gmail.com>
2023-09-26 00:29:27 +0530

    leetcode

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -2,23 +2,23 @@ import java.util.*;
 class BinarySearch {
     int binarySearch(int arr[], int x, int low, int high){
         if(low <= high){
-        int mid= (low+high)/2; //(0+4) / 2 => 2
+        int mid = (low + high) / 2; //(0+4) / 2 => 2
             if(arr[mid] == x){
                 return mid + 1; //3
             }
-            else if(x > arr[mid]){
-                return binarySearch(arr, mid+1, high, x);
+            else if(arr[mid] < x){
+                low = mid + 1;
         }
             else{
-                return binarySearch(arr, low, mid-1, x);
+                high = mid - 1;
         }
     }
     return -1;
 }
        
         public static void main(String[] args){
-            int[] arr = {1,2,3,4,5};
+            int[] arr = {1,0,1,1,1};
 
-            System.out.println("The element is " +new BinarySearch().binarySearch(arr, 3, 0, 4));
+            System.out.println("The element is " +new BinarySearch().binarySearch(arr, 0, 0, 4));
         }
     }
