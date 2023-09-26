9e310ebcccba279ac36d27595cdaaa9d5a7f7e0b
nistha <nisthasharma0799@gmail.com>
2023-09-26 23:15:00 +0530

    leetcode

---
+++ leetcode/Search/sortedRotated1.java
@@ -0,0 +1,38 @@
+package leetcode.Search;
+
+public class sortedRotated1 {
+    public int search(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1; //5
+
+        while(low <= high){
+            int mid = (low + high) / 2; //2 || 0 || 1
+
+            if(nums[mid] == target) return mid + 1; //2
+
+            if(nums[mid] >= nums[low]){ //left is sorted
+                if(nums[mid] > target && target >= nums[low]){
+                    high = mid - 1; // high = 1
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{ //right is sorted 
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid + 1; //low = 1
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {6,7,0,1,2,3};
+        int target = 7;
+
+        System.out.println("The element is " +new sortedRotated1().search(nums, target));
+    }
+}
