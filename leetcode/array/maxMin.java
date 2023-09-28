50ada7e17af03ba054fe1d71d5815151acd9cc97
nistha <nisthasharma0799@gmail.com>
2023-09-28 21:39:59 +0530

    leetcode

--- leetcode/array/maxMin.java
+++ leetcode/array/maxMin.java
@@ -17,7 +17,7 @@ public class maxMin {
             return new int[] {max, min};
     }
     public static void main(String[] args){
-        int arr[] = {4,3,2,1,69,0};
+        int arr[] = {4,3,2,1,9,0};
         int[] result=findMaxMin(arr);
         System.out.println("Maximum and the minimum element is " +Arrays.toString(result));
     } 
