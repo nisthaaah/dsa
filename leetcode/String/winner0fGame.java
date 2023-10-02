63f327f98cfcb73ceee87161a77bece1c9c4900b
nistha <nisthasharma0799@gmail.com>
2023-10-02 17:26:33 +0530

    leetcode

---
+++ leetcode/String/winner0fGame.java
@@ -0,0 +1,26 @@
+package leetcode.String;
+
+public class winner0fGame {
+    public boolean winner(String colors){
+        int alice = 0;
+        int bob = 0;
+
+        for(int j = 1; j < colors.length() - 1; j++){
+            int i = j - 1;
+            int k = j + 1;
+
+            if(colors.charAt(i) == 'A' && colors.charAt(j) == 'A' && colors.charAt(k) == 'A'){
+                alice++;
+            }
+            if(colors.charAt(i) == 'B' && colors.charAt(j) == 'B' && colors.charAt(k) == 'B'){
+               bob++;
+            }
+        }
+        return alice > bob;
+    }
+
+    public static void main(String[] args) {
+        String colors = "AAABABB";
+        System.out.println("The winner of the game is " +new winner0fGame().winner(colors));
+    }
+}
