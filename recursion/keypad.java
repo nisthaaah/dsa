915794da107598f092e10c8adbfb7ead71e11525
nistha <nisthasharma0799@gmail.com>
2023-10-07 00:24:43 +0530

    recursion

---
+++ recursion/keypad.java
@@ -0,0 +1,23 @@
+package recursion;
+
+public class keypad {
+    public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+    public static void printKeyPad(String s, int i, String combination){
+        if(i == s.length()){
+            System.out.print(combination + " ");
+            return;
+        }
+
+        char curr = s.charAt(i);
+        String mapping = keypad[curr - '0'];
+
+        for(int j = 0; j < mapping.length(); j++){
+            printKeyPad(s, i + 1, combination + mapping.charAt(j));
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "13";
+        printKeyPad(s, 0, "");
+    }
+}
---
+++ recursion/subsequencesUnique.java
@@ -0,0 +1,31 @@
+package recursion;
+import java.util.HashSet;
+import java.util.Set;
+
+public class subsequencesUnique {
+    public static void subsequencesUnique(String s, int i, String newStr, Set<String> set){
+        if(i == s.length()){
+            if(set.contains(newStr)){
+                return;
+            }
+            else{
+                System.out.print(newStr + " ");
+                set.add(newStr);
+                return;
+            }
+        }
+
+        char curr = s.charAt(i);
+        //to be
+        subsequencesUnique(s, i + 1, newStr + curr, set);
+
+        //not to be
+        subsequencesUnique(s, i + 1, newStr, set);
+    }
+
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        String s = "aaa";
+        subsequencesUnique(s, 0, "", set);
+    }
+}
