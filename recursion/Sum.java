eda19ba1ff5a03ce07eb8573ab068e17dae8335d
nistha <nisthasharma0799@gmail.com>
2023-10-06 16:38:26 +0530

    recursion

--- recursion/Sum.java
+++ recursion/Sum.java
@@ -1,15 +1,29 @@
 package recursion;
 
 public class Sum {
-    public static int printsum(int n){
-        if(n != 0){
-            return n + printsum(n-1);
+    public static int sum(int n){
+        if(n > 0){
+            return n + sum(n - 1);
+        }
+        else{
+            return 0;
+        }
     }
-    else return n;
-}
-    public static void main(String args[]){
-        System.out.println("The sum is: " +printsum(4));
-
-    }
-    
+    public static void main(String[] args) {
+            System.out.println("The sum is: " +sum(5));
+        }
+//     public static void printsum(int i, int n, int sum){
+//         if(i == n){
+//             sum += i;
+//             System.out.println(sum);
+//             return;
+           
+//     }
+//     sum += i;
+//     printsum(i + 1, n, sum);
+//     System.out.println(i);
+// }
+//     public static void main(String args[]){
+//         printsum(1, 5, 0);
+//     }    
 }
