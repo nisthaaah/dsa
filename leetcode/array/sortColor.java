caaba011093563c93e75d5cd6ed7e662ec6162b8
nistha <nisthasharma0799@gmail.com>
2023-10-15 15:49:27 +0530

    leetcode

---
+++ leetcode/array/sortColor.java
@@ -0,0 +1,39 @@
+package leetcode.Array;
+
+public class sortColor {
+    public static void sort(int[] nums){
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,0,2,1,1,0};
+        sort(nums);
+        System.out.println("After sorting: ");
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+}
