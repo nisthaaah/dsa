6460f793561ac57291d7b12d8cb68327f559d75e
nistha <nisthasharma0799@gmail.com>
2023-10-16 22:31:47 +0530

    leetcode

--- leetcode/Patterns/pascalsTriangle.java
+++ leetcode/Patterns/pascalsTriangle.java
@@ -1,5 +1,6 @@
 package leetcode.Patterns;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class pascalsTriangle {
     public List<List<Integer>> pascT(int numRows){
@@ -9,11 +10,11 @@ public class pascalsTriangle {
         for(int i = 0; i < numRows; i++){ // 0 < 4
             List<Integer> row = new ArrayList<>();
             for(int j = 0; j <= i; j++ ){ //0 <= 4
-                if(j==0 || j == i){                                                      //  1 
+                if(j == 0 || j == i){                                                    //  1
                     row.add(1);                                                       // 1   1
                 }                                                                      // 1  2  1 [1st row(2nd element) + 1st row(1st element)]
                 else{                                                                 // 1  3  3  1
-                    row.add(result.get(i-1).get(j) + result.get(i-1).get(j-1)); 
+                    row.add(result.get(i-1).get(j) + result.get(i-1).get(j-1)); //result.get(1).get(1) + result.get(1).get(0) => 1 + 1 -> 2
                 }
             }
             result.add(row);
