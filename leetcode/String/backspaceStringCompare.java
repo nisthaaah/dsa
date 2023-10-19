aa40643f02b5ed519db23e06d235014cf430b761
nistha <nisthasharma0799@gmail.com>
2023-10-19 21:50:00 +0530

    leetcode

--- leetcode/String/backspaceStringCompare.java
+++ leetcode/String/backspaceStringCompare.java
@@ -33,7 +33,7 @@ public class backspaceStringCompare {
 
     public static void main(String[] args) {
         String s = "ab#c";
-        String t = "ab";
+        String t = "abc#";
         System.out.println("The Strings are equal: " +new backspaceStringCompare().backspaceString(s, t));
     }
 }
