ba5ae7186458b21f5394a5083e86d68c9b2c5ea5
nistha <nisthasharma0799@gmail.com>
2023-10-23 19:22:09 +0530

    leetcode

---
+++ leetcode/Random/isPower/isPower3.java
@@ -0,0 +1,15 @@
+package leetcode.Random.isPower;
+
+public class isPower3 {
+    public boolean isPower3(int n){
+        if(n <= 0) return false;
+        while(n % 3 == 0){
+            n /= 3;
+        }
+        return n == 1;
+    }
+    public static void main(String[] args) {
+        int n = 27;
+        System.out.println("The integer " +n+ " is a power of 3: " +new isPower3().isPower3(n));
+    }
+}
