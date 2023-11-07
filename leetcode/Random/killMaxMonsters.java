108b0a0199ee0043d27709f2f120697ca62a1a52
nistha <nisthasharma0799@gmail.com>
2023-11-07 23:59:21 +0530

    leetcode

--- leetcode/Random/killMaxMonsters.java
+++ leetcode/Random/killMaxMonsters.java
@@ -7,10 +7,10 @@ public class killMaxMonsters {
         double[] time = new double[n];
 
         for(int i = 0; i < n; i++){
-            time[i] = (double)dist[i] / speed[i]; //[1,1,2,3]
+            time[i] = (double)dist[i] / speed[i]; //[1,1,2,3] || [0.6, 2, 1]
         }
 
-        Arrays.sort(time); //[1,1,2,3] || [0.6, 0.67, 2]
+        Arrays.sort(time); //[1,1,2,3] || [0.6, 1, 2]
 
         for(int i = 0; i < n; i++){
             if(time[i] <= i){
@@ -20,8 +20,8 @@ public class killMaxMonsters {
         return n;
     }
     public static void main(String[] args) {
-        int[] dist = {3,2,2};
-        int[] speed = {5,3,2};
+        int[] dist = {3,2,2}; //{3,6,2}
+        int[] speed = {5,3,2}; //{5,3,2}
 
         System.out.println("The max number of monsters killed: " +new killMaxMonsters().kill(dist, speed));
     }
