19f59044e56024a77638317025a4191e0b8630ff
nistha <nisthasharma0799@gmail.com>
2023-11-19 23:41:35 +0530

    leetcode

--- patterns/palindromic_pyramid.java
+++ patterns/palindromic_pyramid.java
@@ -9,10 +9,10 @@ public class palindromic_pyramid{
             for(int j=1; j<=n-i; j++){ //4-1 = 3 || 2 || 1
                 System.out.print(" ");
             }
-            for(int j=i; j>=1; j--){ //1 || 2,1 || 3,2,1
+            for(int j=i; j>=1; j--){ //1 || 2, 1 || 3, 2, 1
                 System.out.print(j);
             }
-            for(int j=2; j<=i; j++){ //  || 2 || 2,3
+            for(int j=2; j<=i; j++){ //  || 2 || 2, 3
                 System.out.print(j);
             }
             System.out.println();
