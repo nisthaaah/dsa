58d75837a6796ffdf23d0cead82325255c81f2c9
nistha <nisthasharma0799@gmail.com>
2023-11-19 18:36:15 +0530

    leetcode

--- patterns/palindromic_pyramid.java
+++ patterns/palindromic_pyramid.java
@@ -4,14 +4,15 @@ public class palindromic_pyramid{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the value of n: ");
         int n=sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i; j++){
+
+        for(int i=1; i<=n; i++){ //1 || 2 || 3
+            for(int j=1; j<=n-i; j++){ //4-1 = 3 || 2 || 1
                 System.out.print(" ");
             }
-            for(int j=i; j>=1; j--){
+            for(int j=i; j>=1; j--){ //1 || 2,1 || 3,2,1
                 System.out.print(j);
             }
-            for(int j=2; j<=i; j++){
+            for(int j=2; j<=i; j++){ //  || 2 || 2,3
                 System.out.print(j);
             }
             System.out.println();
