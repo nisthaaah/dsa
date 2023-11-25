7dc7c18581c005bf38a672a4e9f457e0bead84c4
nistha <nisthasharma0799@gmail.com>
2023-11-25 15:34:49 +0530

    leetcode

--- recursion/powerHeightlogn.java
+++ recursion/powerHeightlogn.java
@@ -2,11 +2,12 @@ package recursion;
 import java.util.Scanner;
 
 class powerHeightlogn {
-    public static int calcPower(int base, int exponent){
+    public static int calcPower(int base, int exponent){ //(2, 10)
         if(base == 0) return 0;
         if(exponent == 0) return 1;
 
-        int half = calcPower(base, exponent / 2);
+        int half = calcPower(base, exponent / 2); //exponents => (10 -> 5 -> 2 -> 1 -> 0)
+        //System.out.println(half);
         if(exponent % 2 == 0){ //even power
             return half * half;
         }
