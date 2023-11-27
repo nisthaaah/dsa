9ac08f639e0c6d905691bc5d53c0dac48bcf2d9e
nistha <nisthasharma0799@gmail.com>
2023-11-27 19:32:46 +0530

    leetcode

--- leetcode/Random/isPower/isPower4.java
+++ leetcode/Random/isPower/isPower4.java
@@ -9,7 +9,7 @@ public class isPower4 {
     }
 
     public static void main(String[] args) {
-        int n = 16;
+        int n = 18;
         System.out.println("The integer " +n+ " is a power of 4: " +new isPower4().isPower4(n));
     }
 }
