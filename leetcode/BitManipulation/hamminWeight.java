17a816ecb4fc7fd09a87446ad131e9550c2584ff
nistha <nisthasharma0799@gmail.com>
2023-11-29 21:44:49 +0530

    leetcode

--- leetcode/BitManipulation/hamminWeight.java
+++ leetcode/BitManipulation/hamminWeight.java
@@ -3,7 +3,7 @@ package leetcode.BitManipulation;
 public class hamminWeight {
     public int hamminWeight(int n){
         int count = 0;
-        for(int i = 0; i <32; i++){
+        for(int i = 0; i < 32; i++){
             if((n & 1) == 1){
                 count++;
             }
@@ -13,10 +13,10 @@ public class hamminWeight {
     }
     public static void main(String[] args){
         hamminWeight h = new hamminWeight();
-        int n = 11;
+        int n = 2; //
         //String binaryInput = "10110000000000000";
         //int n = Integer.parseUnsignedInt(binaryInput, 2); //radix:2 indicates that the input is in binary format.
-       
+
         System.out.println("Input: " +n);
         System.out.println("The number of '1' bits: " +h.hamminWeight(n));
         //System.out.println("Reversed: " + b.reverseBits(n));
