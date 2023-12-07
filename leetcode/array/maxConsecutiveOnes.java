ccf08a0d1b34f6a5d38dfcc34c86543d20b50c90
nistha <nisthasharma0799@gmail.com>
2023-12-07 23:49:42 +0530

    leetcode

--- leetcode/array/maxConsecutiveOnes.java
+++ leetcode/array/maxConsecutiveOnes.java
@@ -4,7 +4,7 @@ public class maxConsecutiveOnes {
     public int maxConsecutiveOnes(int[] arr){
         int count = 0;
         int maxCount = 0;
-        for(int i = 0; i < arr.length; i++){ 
+        for(int i = 0; i < arr.length; i++){
             if(arr[i] == 1) count++;
             else count = 0;
 
@@ -14,7 +14,7 @@ public class maxConsecutiveOnes {
     }
 
     public static void main(String[] args){
-        int[] arr = {1,0,1,1,0,1,2};
+        int[] arr = {1,0,1,1,1,0,1,2};
         System.out.println("The max consecutive number of ones are: " +new maxConsecutiveOnes().maxConsecutiveOnes(arr));
     }
 }
