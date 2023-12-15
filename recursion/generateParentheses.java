099f613f06880391803a34a14d058d54492e623b
nistha <nisthasharma0799@gmail.com>
2023-12-15 20:40:37 +0530

    leetcode

--- recursion/generateParentheses.java
+++ recursion/generateParentheses.java
@@ -22,7 +22,7 @@ public class generateParentheses {
         }
     }
     public static void main(String[] args){
-        int n = 3;
+        int n = 2;
         System.out.println("The generated parentheses for " +n+ " are: " +new generateParentheses().generateParentheses(n));
     }
 }
