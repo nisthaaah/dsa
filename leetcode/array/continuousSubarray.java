c36920863881229a05495791beb6c94ce1963b38
nistha <nisthasharma0799@gmail.com>
2023-12-20 22:58:49 +0530

    leetcode

--- leetcode/array/continuousSubarray.java
+++ leetcode/array/continuousSubarray.java
@@ -9,15 +9,15 @@ public class continuousSubarray {
 
         int sum = 0;
         for(int i = 0; i < arr.length; i++){
-            sum += arr[i];
+            sum += arr[i]; //23 || 25 || 29
 
-            if(numMap.containsKey(sum % k)){
-                if(numMap.get(sum % k) < i){
+            if(numMap.containsKey(sum % k)){ //23 % 6 => 5 || 1 || 5 -> map already contains 5 at index 1,
+                if(numMap.get(sum % k) < i){                          //which means there's a subarray from index 1 to index 2 whose sum is a multiple of 6
                     return true;
                 }
             }
                 else{
-                    numMap.put(sum % k, i + 1);
+                    numMap.put(sum % k, i + 1); //5, 1 || 1, 2
                 }
             }
         return false;
